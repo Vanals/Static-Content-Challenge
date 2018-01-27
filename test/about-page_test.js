1b4081c15d881f49956f01ab58cc6937f779867a
Marco Vanali <Marco91Vanali@gmail.com>
2018-01-27 11:44:39 +0000

    Set up correctly about-page_test.js + test expect 200 green

--- test/about-page_test.js
+++ test/about-page_test.js
@@ -1,8 +1,22 @@
 var request = require("supertest");
 var assert = require('assert');
 
-it('should return Ok when request /about-page', function(done) {
-  request(app)
-  .get('/about-page')
-  .expect(200, done);
-});
+describe('routes', function() {
+  var server
+
+  beforeEach(function() {
+    // Clears the cache so a new server instance is used for each test.
+    delete require.cache[require.resolve('../index')];
+    server = require("../index");
+  });
+
+  afterEach(function() {
+      server.close();
+  });
+
+  it('should return Ok when request /about-page', function(done) {
+    request(server)
+    .get('/about-page')
+    .expect(200, done);
+  });
+})
