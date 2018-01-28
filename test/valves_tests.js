22472b2272b2eb6ed146dd3ae47716a58fd901d7
Marco Vanali <Marco91Vanali@gmail.com>
2018-01-28 11:59:03 +0000

    Test that checks the body content of the /valves route fixed
    Test green.

--- test/valves_tests.js
+++ test/valves_tests.js
@@ -23,13 +23,13 @@ describe('routes', function() {
   it('should return the correct body content', function(done) {
     const bodyValves = "<!doctype html>\n" +
     "<html>\n" +
-        "<head>\n" +
-            "<title>Welcome to Acme</title>\n" +
-        "</head>\n" +
-        "<body>\n" +
-          "<h1>Valves</h1>\n" +
-          "<p>Acme Co. valves are amongst the highest quality in the industry. Whether it&#39;s for industrial, commercial, medical or space exploration, you can always count on an Acme Co. valve.</p>\n" +
-        "</body>" +
+    "    <head>\n" +
+    "        <title>Welcome to Acme</title>\n" +
+    "    </head>\n" +
+    "    <body>\n" +
+    "      <h1>Valves</h1>\n" +
+    "      <p>Acme Co. valves are amongst the highest quality in the industry. Whether it&#39;s for industrial, commercial, medical or space exploration, you can always count on an Acme Co. valve.</p>\n" +
+    "    </body>\n" +
     "</html>\n"
 
     request(server)
@@ -38,5 +38,5 @@ describe('routes', function() {
       assert.equal(res.text, bodyValves);
       done();
     });
-  });  
+  });
 })
