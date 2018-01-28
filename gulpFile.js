0e68720e4d85c2c42a56935b8754dbebd94f7459
Marco Vanali <Marco91Vanali@gmail.com>
2018-01-28 11:43:35 +0000

    Create index.html file for /valves folder
    It contain the templace of the template.html file and the content of
    the /valves/index.md file converted in html.

--- gulpFile.js
+++ gulpFile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const markdown = require('gulp-markdown');
 
 gulp.task('default', () =>
-    gulp.src('./content/jobs/index.md')
+    gulp.src('./content/valves/index.md')
         .pipe(markdown())
         .pipe(gulp.dest(''))
 );
