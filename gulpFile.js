76ed8259008f6b9a1109b41ee34a65a7720b3fa1
Marco Vanali <Marco91Vanali@gmail.com>
2018-01-28 10:54:49 +0000

    Create index.html file in the content/jobs folder
    This index.html file contain the template.html code and the index.md content converted in html

--- gulpFile.js
+++ gulpFile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const markdown = require('gulp-markdown');
 
 gulp.task('default', () =>
-    gulp.src('./content/about-page/index.md')
+    gulp.src('./content/jobs/index.md')
         .pipe(markdown())
         .pipe(gulp.dest(''))
 );
